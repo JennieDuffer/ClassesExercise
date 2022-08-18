d46a8318d8e8f7216675cee7147c7ed802d51ca8
Jennie Duffer <jenniej297@gmail.com>
2022-08-18 11:48:50 -0500

    initial commit

---
+++ ClassesExercise/Program.cs
@@ -0,0 +1,10 @@
+﻿using ClassesExercise;     
+
+Car myCar = new Car("Jeep", "Grand Cherokee", 2017);
+
+Console.WriteLine($" Make: {myCar.Make}");
+Console.WriteLine($" Model: {myCar.Model}");
+Console.WriteLine($" Year: {myCar.Year}");
+
+
+
